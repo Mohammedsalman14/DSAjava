e6e2341abb794fb2d97efbd413f4664308933754
Mohammedsalman14 <mohamedsalman140@gmail.com>
2023-06-22 12:11:03 +0530

    Instance overloading

---
+++ oops/Inheritance/Main.java
@@ -0,0 +1,8 @@
+package oops.Inheritance;
+
+public class Main {
+    public static void main(String[] args) {
+        Box b1=new Box();
+        System.out.println(b1.l+" "+b1.h+" "+b1.w);
+    }
+}
